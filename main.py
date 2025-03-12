66c177cceba17e136f38a5a5afb280b9e746b3e7
kimdaewhi <kimdaewhi@naver.com>
2025-03-12 09:01:14 +0900

    main.py 주석 추가가

--- main.py
+++ main.py
@@ -4,25 +4,33 @@ from utils.data_loader import load_stock_data
 from strategy_factory import StrategyFactory
 from config import Config
 
-# ✅ 주가 데이터 로드
+# 1. 주가 데이터 로드
 df = load_stock_data()
 prices = df["Close"].values
 dates = df["Date"].tolist()  # ✅ datetime을 문자열로 변환
 
-# ✅ 전략 실행 (손절 후 재매수 & DCA)
+# 2. 전략 실행 (손절 후 재매수 & DCA), 다수의 전략 실행 가능
 strategy_names = ["stop_loss_rebuy", "dca"]
 results = {}
 
+# 3. 전략 오브젝트 생성 및 실행
 for name in strategy_names:
-    strategy = StrategyFactory.create_strategy(name, initial_investment=Config.INITIAL_INVESTMENT, avg_price=Config.AVG_PRICE ,shares=Config.SHARES)
-    results[name] = strategy.run(prices)
+    # Factory 클래스에 위임한 전략 생성. 파라미터는 환경 변수에 정의된 초기값 사용
+    strategy = StrategyFactory.create_strategy(
+        name, 
+        initial_investment=Config.INITIAL_INVESTMENT,   # 초기 투자금
+        avg_price=Config.AVG_PRICE,                     # 평균 매수가
+        shares=Config.SHARES                            # 보유 주식 수량
+    )
+    results[name] = strategy.run(prices)            # 전략 실행 결과를 Dictionary에 저장
 
-# ✅ Plotly 그래프 시각화
+
+# 4. Plotly 그래프 시각화(Plotly 사용법?)
 fig = go.Figure()
 for name, values in results.items():
     fig.add_trace(go.Scatter(x=dates, y=values, mode="lines", name=name))
 
-st.title("Oracle (ORCL) Backtest Results")
+st.title(f"Backtest Results{Config.TICKER}")
 st.plotly_chart(fig, use_container_width=True)
 
 # ✅ 전략별 백테스트 성과 지표 테이블
